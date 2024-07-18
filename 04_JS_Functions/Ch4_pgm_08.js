8714d7a925024289fd22fea488f496a086e2d00f
SweetyMercyD <sweetymercydavid@gmail.com>
2024-07-18 13:45:27 +0530

    js function

---
+++ 04_JS_Functions/Ch4_pgm_08.js
@@ -0,0 +1,43 @@
+// Displaying a menu
+
+var displayMenu;
+
+displayMenu = function () {
+	console.log("Please choose an option:");
+	console.log("(1) Print log");
+	console.log("(2) Upload file");
+	console.log("(3) Download file"); 
+    console.log("(4) Delete file");   
+	console.log("(9) Quit");
+};
+
+// Call the displayMenu function
+displayMenu();
+
+// Create your own displayMyMenu function
+var displayMyMenu;
+
+displayMyMenu = function () {
+    console.log("Welcome to My Menu!");
+    console.log("(1) Start a new game");
+    console.log("(2) Load saved game");
+    console.log("(3) View high scores");
+    console.log("(4) Settings");
+    console.log("(9) Exit");
+};
+
+displayMyMenu();
+
+
+
+/* Further Adventures
+ *
+ * 1) Add two more options to the displayMenu
+ *    function and run the program.
+ *
+ * 2) Create your own displayMyMenu function.
+ *
+ * 3) Add a line to call your function as well.
+ *    Run the program
+ *
+ */
